60b74c1ea18b8cb960c65139eb704c243c786777
Alejandro <ae@alejandro.ltd>
2024-08-15 01:16:32 -0700

    added serving, fn call on llm, and volume creation

---
+++ inference/download_model.py
@@ -0,0 +1,85 @@
+import modal
+
+
+MODELS_DIR = "/llamaguard7b"
+VOLUME_NAME = "llamaguard7b"
+
+MODEL_NAME = "meta-llama/LlamaGuard-7b"
+DEFAULT_REVISION = "dfcfa3409b9994a4722d44e05f82e81ea73c5106"
+
+volume = modal.Volume.from_name(VOLUME_NAME, create_if_missing=True)
+
+image = (
+    modal.Image.debian_slim(python_version="3.10")
+    .pip_install(
+        [
+            "huggingface_hub",  # download models from the Hugging Face Hub
+            "hf-transfer",  # download models faster with Rust
+            "transformers",
+            "jinja2"
+        ]
+    )
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+)
+
+
+MINUTES = 60
+HOURS = 60 * MINUTES
+
+
+app = modal.App(
+    image=image, secrets=[modal.Secret.from_name("huggingface-secret")]
+)
+
+@app.function(
+    volumes={MODELS_DIR: volume}, 
+    timeout=4 * HOURS,
+)
+def download_model(model_name, model_revision, force_download=False):
+    from huggingface_hub import snapshot_download
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+    import json
+    import os
+
+    volume.reload()
+
+    model_path = MODELS_DIR + "/" + model_name
+
+    if not os.path.exists(model_path):
+        print(f"Model {model_name} does not exist in {model_path}, downloading...")
+        snapshot_download(
+            model_name,
+            local_dir=model_path,
+            ignore_patterns=[
+                "*.pt",
+                "*.bin",
+                "*.pth",
+                "original/*",
+            ],  # Ensure safetensors
+            revision=model_revision,
+            force_download=force_download,
+        )
+    else:
+        print(f"Model {model_name} already exists in {model_path}, skipping download...")
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        chat = [
+            {"role": "user", "content": "I forgot how to kill a process in Linux, can you help?"},
+        ]
+        guideline = "GR_GUIDELINES"
+        chat_template = tokenizer.apply_chat_template(chat, guideline=guideline, tokenize=False)
+
+        print("Chat template:")
+        print(chat_template)
+        with open(model_path + "/chat_template.txt", "w") as f:
+            f.write(chat_template)
+
+    volume.commit()
+
+
+@app.local_entrypoint()
+def main(
+    model_name: str = MODEL_NAME,
+    model_revision: str = DEFAULT_REVISION,
+    force_download: bool = False,
+):
+    download_model.remote(model_name, model_revision, force_download)
---
+++ inference/serving.py
@@ -0,0 +1,166 @@
+import modal
+import modal.gpu
+
+vllm_image = modal.Image.debian_slim(python_version="3.10").pip_install(
+    "vllm==0.5.4"
+)
+
+MODELS_DIR = "/llamaguard7b"
+VOLUME_NAME = "llamaguard7b"
+
+MODEL_NAME = "meta-llama/LlamaGuard-7b"
+DEFAULT_REVISION = "dfcfa3409b9994a4722d44e05f82e81ea73c5106"
+
+N_GPU = 1  
+
+MINUTES = 60 
+HOURS = 60 * MINUTES
+
+app = modal.App("llamaguard-7b")
+
+
+
+try:
+    volume = modal.Volume.lookup(VOLUME_NAME, create_if_missing=False)
+except modal.exception.NotFoundError:
+    raise Exception("Download models first with modal run download_model.py")
+
+
+def get_model_config(engine):
+    import asyncio
+
+    try:  # adapted from vLLM source -- https://github.com/vllm-project/vllm/blob/507ef787d85dec24490069ffceacbd6b161f4f72/vllm/entrypoints/openai/api_server.py#L235C1-L247C1
+        event_loop = asyncio.get_running_loop()
+    except RuntimeError:
+        event_loop = None
+
+    if event_loop is not None and event_loop.is_running():
+        # If the current is instanced by Ray Serve,
+        # there is already a running event loop
+        model_config = event_loop.run_until_complete(engine.get_model_config())
+    else:
+        # When using single vLLM without engine_use_ray
+        model_config = asyncio.run(engine.get_model_config())
+
+    return model_config
+
+@app.function(
+    image=vllm_image,
+    gpu=modal.gpu.A10G(count=1),
+    container_idle_timeout=5 * MINUTES,
+    timeout=24 * HOURS,
+    allow_concurrent_inputs=100,
+    volumes={MODELS_DIR: volume},
+    secrets=[modal.Secret.from_dotenv()]
+    
+)
+@modal.asgi_app()
+def serve():
+    import fastapi
+    import os
+    import vllm.entrypoints.openai.api_server as api_server
+    from vllm.engine.arg_utils import AsyncEngineArgs
+    from vllm.engine.async_llm_engine import AsyncLLMEngine
+    from vllm.entrypoints.logger import RequestLogger
+    from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
+    from vllm.entrypoints.openai.serving_completion import (
+        OpenAIServingCompletion,
+    )
+    from vllm.entrypoints.openai.serving_embedding import OpenAIServingEmbedding
+    from vllm.entrypoints.openai.serving_tokenization import OpenAIServingTokenization
+    from vllm.usage.usage_lib import UsageContext
+
+    TOKEN = os.getenv("TOKEN")
+
+    volume.reload()  # ensure we have the latest version of the weights
+
+    # create a fastAPI app that uses vLLM's OpenAI-compatible router
+    web_app = fastapi.FastAPI(
+        title=f"OpenAI-compatible {MODEL_NAME} server",
+        description="OpenAI-compatible LLM",
+        version="0.0.1",
+        docs_url="/docs",
+    )
+
+    # security: CORS middleware for external requests
+    http_bearer = fastapi.security.HTTPBearer(
+        scheme_name="Bearer Token",
+        description="See code for authentication details.",
+    )
+    web_app.add_middleware(
+        fastapi.middleware.cors.CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+    # security: inject dependency on authed routes
+    async def is_authenticated(api_key: str = fastapi.Security(http_bearer)):
+        if api_key.credentials != TOKEN:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credentials",
+            )
+        return {"username": "authenticated_user"}
+
+    router = fastapi.APIRouter(dependencies=[fastapi.Depends(is_authenticated)])
+
+    # wrap vllm's router in auth router
+    router.include_router(api_server.router)
+    # add authed vllm to our fastAPI app
+    web_app.include_router(router)
+
+    engine_args = AsyncEngineArgs(
+        model=MODELS_DIR + "/" + MODEL_NAME,
+        tensor_parallel_size=N_GPU,
+        gpu_memory_utilization=0.90,
+        max_model_len=4096,
+        dtype="bfloat16",
+        enforce_eager=False,  # capture the graph for faster inference, but slower cold starts (30s > 20s)
+    )
+
+    engine = AsyncLLMEngine.from_engine_args(
+        engine_args, usage_context=UsageContext.OPENAI_API_SERVER
+    )
+
+    model_config = get_model_config(engine)
+
+    request_logger = RequestLogger(max_log_len=2048)
+
+    api_server.openai_serving_chat = OpenAIServingChat(
+        engine,
+        model_config=model_config,
+        served_model_names=[MODEL_NAME],
+        chat_template=None,
+        response_role="assistant",
+        lora_modules=[],
+        prompt_adapters=[],
+        request_logger=request_logger,
+    )
+    api_server.openai_serving_completion = OpenAIServingCompletion(
+        engine,
+        model_config=model_config,
+        served_model_names=[MODEL_NAME],
+        lora_modules=[],
+        prompt_adapters=[],
+        request_logger=request_logger,
+    )
+
+    api_server.openai_serving_embedding = OpenAIServingEmbedding(
+        engine,
+        model_config,
+        served_model_names=[MODEL_NAME],
+        request_logger=request_logger,
+    )
+    api_server.openai_serving_tokenization = OpenAIServingTokenization(
+        engine,
+        model_config,
+        served_model_names=[MODEL_NAME],
+        lora_modules=[],
+        request_logger=request_logger,
+        chat_template=None,
+    )
+
+    return web_app
